32d2af1586e455a710ce2cf57d05d99d69357360
Magi123tlm <66484013+Magi123tlm@users.noreply.github.com>
2025-02-11 19:14:28 +0530

    Update Navbar.jsx and changed extension

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.jpg";
 import { Link, useNavigate } from "react-router-dom";
 import Avatar from "react-avatar";
 import { MdLightMode } from "react-icons/md";
